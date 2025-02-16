ba0080ef6bee86ae0287b8ffd6a0a0502d4acc23
andropol1 <andrew.kondratenkov@gmail.com>
2025-02-16 15:16:42 +0300

    port for kafka was changed

--- dispatcher/src/main/java/ru/andropol1/config/KafkaConfiguration.java
+++ dispatcher/src/main/java/ru/andropol1/config/KafkaConfiguration.java
@@ -34,7 +34,7 @@ public class KafkaConfiguration {
 	@Bean
 	public ProducerFactory<String, Update> producerFactory() {
 		Map<String, Object> configProps = new HashMap<>();
-		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9093");
 		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 		return new DefaultKafkaProducerFactory<>(configProps);
@@ -43,7 +43,7 @@ public class KafkaConfiguration {
 	@Bean
 	public ConsumerFactory<String, SendMessage> consumerFactory() {
 		Map<String, Object> props = new HashMap<>();
-		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9093");
 		props.put(ConsumerConfig.GROUP_ID_CONFIG, "telegram");
 		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
